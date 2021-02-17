7707ef262e581e1f94cdbe52f3ab8bff20baddd7
abdulmoizshaikh <muhammadmoizshaikh@gmail.com>
2021-02-17 19:42:16 +0500

    dynamic styling with inline styles

--- app.js
+++ app.js
@@ -1,81 +1,29 @@
-// <!-- 02 understand event binding -->
-
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
-      name: "",
-      confirmName: "",
-      completeName: "",
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
     };
   },
-  watch: {
-    number(newVal, oldVal) {
-      alert("number changed");
-    },
-  },
-  computed: {
-    number() {
-      return this.name + " " + "lastname";
-    },
-  },
-
   methods: {
-    add(num) {
-      this.counter += num;
-    },
-    subtract(num) {
-      this.counter -= num;
-    },
-    outputFullName() {
-      console.log("outputFullName");
-      if (this.name === "") {
-        return "";
-      }
-      return this.name + " " + "Shah";
-    },
-    // setName(event, surname) {
-    //   // this.name = event.target.value + surname;
-    //   this.name = event.target.value;
-    // },
-    submitForm(event) {
-      alert("submit");
-    },
-    confirmInput() {
-      this.confirmName = this.name;
-    },
-    resetInput() {
-      this.name = "";
-    },
-  },
-});
-
-app.mount("#events");
-
-// <!-- basics-01-starting-code -->
-/* const app = Vue.createApp({
-  //  here data is function
-  data() {
-    return {
-      courseGoal: "Course Goal",
-      courseGoalA: "Finish the Basic course of Vue!",
-      courseGoalB: "Finish the Master course of Vue!",
-      courseGoalC: "<h3>Course Goal HTML interpreted</h3>",
-      vueLink: "https://vuejs.org/",
-    };
-  },
-  //methods is an object
-  methods: {
-    outputGoal() {
-      let randomNumber = Math.random();
-      if (randomNumber < 0.5) {
-        return this.courseGoalA;
-      } else {
-        return this.courseGoalC;
+    boxSelected(boxId) {
+      console.log("boxId", boxId);
+      switch (boxId) {
+        case "A":
+          this.boxASelected = true;
+          break;
+        case "B":
+          this.boxBSelected = true;
+          break;
+        case "C":
+          this.boxCSelected = true;
+          break;
+        default:
+          break;
       }
     },
   },
 });
 
-app.mount("#user-goal"); //to establish connection of Vue to html page
- */
+app.mount("#styling");
